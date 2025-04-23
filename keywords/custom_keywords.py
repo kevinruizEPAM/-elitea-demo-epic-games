53a01982f86b2aefe4d5f0bcfbb5194af0cbc8e3
kevinruizEPAM <kevin_ruiz1@epam.com>
2025-04-23 11:15:38 -0500

    Update keywords/custom_keywords.py

--- keywords/custom_keywords.py
+++ keywords/custom_keywords.py
@@ -5,4 +5,10 @@ from robot.libraries.BuiltIn import BuiltIn
 def verify_element_text(locator, expected_text):
     seleniumlib = BuiltIn().get_library_instance('SeleniumLibrary')
     actual_text = seleniumlib.get_text(locator)
-    BuiltIn().should_be_equal(actual_text, expected_text)
+    BuiltIn().should_be_equal(actual_text, expected_text
+
+def verify_wishlist_activity_trends():
+    trends_element = driver.find_element_by_xpath("//div[@id='wishlist-activity-trends']")
+    trends_text = trends_element.text
+    assert "Additions" in trends_text, "Additions not found in wishlist activity trends"
+    assert "Removals" in trends_text, "Removals not found in wishlist activity trends")
